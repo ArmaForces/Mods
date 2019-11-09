89fd2671a3a0344ec7bf3735e156aa02dc778bd9
Filip Maciejewski <veteran29q@gmail.com>
2019-11-09 02:37:58 +0100

    Add 3DEN categories and menu strip entry to main component

---
+++ addons/main/CfgEden.hpp
@@ -0,0 +1,11 @@
+class Cfg3DEN {
+    class Object {
+        class AttributeCategories {
+            class PREFIX {
+                displayName = CSTRING(Attributes);
+                collapsed = 1;
+                class Attributes {};
+            };
+        };
+    };
+};
