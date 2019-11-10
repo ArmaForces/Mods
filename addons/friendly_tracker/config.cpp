af58571817cec77dbb735f168fca68b412a3d64b
3Mydlo3 <mydlo333@interia.pl>
2019-11-10 11:10:32 +0100

    Fix config.cpp COMPONENT_NAME

--- addons/friendly_tracker/config.cpp
+++ addons/friendly_tracker/config.cpp
@@ -2,7 +2,7 @@
 
 class CfgPatches {
     class ADDON {
-        name = COMPONENT;
+        name = COMPONENT_NAME;
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
