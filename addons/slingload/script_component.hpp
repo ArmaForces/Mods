2c815eead1c17bf43d296ab940a3ea828006719d
Filip Maciejewski <veteran29@users.noreply.github.com>
2019-11-25 17:25:32 +0100

    Fix paths in includes

--- addons/slingload/script_component.hpp
+++ addons/slingload/script_component.hpp
@@ -1,10 +1,8 @@
 #define COMPONENT slingload
-#define COMPONENT_BEAUTIFIED Sling-Load
-#include "\x\tac\addons\main\script_mod.hpp"
+#include "\z\afm\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
-// #define ENABLE_PERFORMANCE_COUNTERS
 
 #ifdef DEBUG_ENABLED_SLINGLOAD
     #define DEBUG_MODE_FULL
@@ -14,4 +12,4 @@
     #define DEBUG_SETTINGS DEBUG_SETTINGS_SLINGLOAD
 #endif
 
-#include "\x\tac\addons\main\script_macros.hpp"
+#include "\z\afm\addons\main\script_macros.hpp"
