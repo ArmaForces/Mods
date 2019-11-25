29932295af7c077fe9b0c0915870bdd6ffeb80e8
Filip Maciejewski <veteran29@users.noreply.github.com>
2019-11-25 21:21:55 +0100

    Add slingloading component (#16)
    * Add slingloading component
    
    * Fix paths in includes

---
+++ addons/slingload/README.md
@@ -0,0 +1,8 @@
+## Slingload
+
+Improves sling-loading performance by changing locality of the sling-loaded object to the pilot's machine.
+
+### Authors
+
+- [ARK](http://www.ark-group.org)
+- [Jonpas](http://github.com/jonpas)
---
+++ addons/slingload/script_component.hpp
@@ -0,0 +1,15 @@
+#define COMPONENT slingload
+#include "\z\afm\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_SLINGLOAD
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SLINGLOAD
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SLINGLOAD
+#endif
+
+#include "\z\afm\addons\main\script_macros.hpp"
