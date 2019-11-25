01fdd0e39bc141f8d473514eac2aeacc73f14e19
Filip Maciejewski <veteran29q@gmail.com>
2019-11-25 20:12:40 +0100

    Create difficulty component

---
+++ addons/difficulty/CfgAILevelPresets.hpp
@@ -0,0 +1,9 @@
+class CfgAILevelPresets {
+    class Custom;
+    class GVAR(LevelAI): Custom {
+        displayName = CSTRING(DifficultyName);
+
+        skillAI=0.9;
+        precisionAI=0.2;
+    };
+};
---
+++ addons/difficulty/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT difficulty
+#include "\z\afm\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_DIFFICULTY
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_DIFFICULTY
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_DIFFICULTY
+#endif
+
+#include "\z\afm\addons\main\script_macros.hpp"
