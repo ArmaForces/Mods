9c1ed20441991d4f0f3be4cd5ae2c17c1e0bd97c
Filip Maciejewski <veteran29q@gmail.com>
2020-01-12 16:09:57 +0100

    Fix build on linux

--- addons/main_menu/script_component.hpp
+++ addons/main_menu/script_component.hpp
@@ -1,7 +1,7 @@
 #define COMPONENT main_menu
 #include "\z\afm\addons\main\script_mod.hpp"
 
-#include "\A3\Ui_f\hpp\defineResincl.inc"
+#include "\a3\ui_f\hpp\defineResincl.inc"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
