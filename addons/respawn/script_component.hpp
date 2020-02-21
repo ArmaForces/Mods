e73234d947279e91d32fa1f8f571a5925b4308e9
3Mydlo3 <mydlo333@interia.pl>
2020-02-21 09:01:41 +0100

    Create respawn component

---
+++ addons/respawn/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT respawn
+#include "\z\afm\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_RESPAWN
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_RESPAWN
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_RESPAWN
+#endif
+
+#include "\z\afm\addons\main\script_macros.hpp"
