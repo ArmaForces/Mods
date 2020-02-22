faef1ac1b62345bcfed3e8be7effd0efde557821
3Mydlo3 <mydlo333@interia.pl>
2020-02-22 21:41:21 +0100

    Create fatigue component

---
+++ addons/fatigue/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT fatigue
+#include "\z\afm\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_FATIGUE
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_FATIGUE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_FATIGUE
+#endif
+
+#include "\z\afm\addons\main\script_macros.hpp"
