69a63df1ee5b81886c1568419c31fe2836843e8a
Filip Maciejewski <veteran29q@gmail.com>
2020-03-18 00:33:40 +0100

    Update units[] in CfgPatches

--- addons/insurgents/config.cpp
+++ addons/insurgents/config.cpp
@@ -3,7 +3,11 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {};
+        units[] = {
+            GVAR(soldier_i),
+            GVAR(soldier_o),
+            GVAR(soldier_b)
+        };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
