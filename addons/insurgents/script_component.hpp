6241c053276ccc9b31baff695509b04e64e1b5aa
Filip Maciejewski <veteran29q@gmail.com>
2020-03-18 00:17:56 +0100

    Properly handle empty gear lists

--- addons/insurgents/script_component.hpp
+++ addons/insurgents/script_component.hpp
@@ -16,6 +16,7 @@
 #define EMPTY_LOADOUT [[],[],[],[],[],[],"","",[],["ItemMap","","ItemRadio","ItemCompass","ItemWatch",""]]
 
 #define IS_DISPOSABLE(var) (cba_disposable_replaceDisposableLauncher && {!isNil {cba_disposable_NormalLaunchers getVariable var}})
+#define RANDOM_GEAR(var) (GVAR(randomGear) getVariable [#var,[]])
 
 #define CHANCE_HEADGEAR 0.5
 #define CHANCE_FACEWEAR 0.3
