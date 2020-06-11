ec85a64727dc160b5220eb109bf7689392dbf2bb
Filip Maciejewski <veteran29@users.noreply.github.com>
2020-06-11 02:48:13 +0200

    Add pointing component

---
+++ addons/pointing/README.md
@@ -0,0 +1,7 @@
+## Pointing
+
+Enchancment for ACE Finger component. Allows to point directions in vehicles. Currently directions can only be shown on DUI Squad Radar
+
+### Authors
+
+- [veteran29](http://github.com/veteran29)
---
+++ addons/pointing/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT pointing
+#include "\z\afm\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_POINTING
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_POINTING
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_POINTING
+#endif
+
+#include "\z\afm\addons\main\script_macros.hpp"
