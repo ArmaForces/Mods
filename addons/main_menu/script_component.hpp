1a5c154fd10b904f6e4df52e6e7f02f6b18acd0b
Filip Maciejewski <veteran29q@gmail.com>
2020-06-25 16:40:36 +0200

    Disable debug

--- addons/main_menu/script_component.hpp
+++ addons/main_menu/script_component.hpp
@@ -4,7 +4,7 @@
 #include "\a3\ui_f\hpp\defineCommonGrids.inc"
 #include "\a3\ui_f\hpp\defineResincl.inc"
 
-#define DEBUG_MODE_FULL
+// #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_MAIN_MENU
