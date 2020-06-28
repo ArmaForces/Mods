ba96393162794d717114551f99e00ef9cb905bf2
Filip Maciejewski <veteran29q@gmail.com>
2020-06-28 15:58:36 +0200

    Add wiki link to editor toolbar

--- addons/main/DisplayEden.hpp
+++ addons/main/DisplayEden.hpp
@@ -6,6 +6,15 @@ class Display3DEN {
                 items[] += {QUOTE(PREFIX)};
                 class PREFIX {
                     text = CSTRING(Toolbar);
+                    items[] += {
+                        QGVAR(Wiki)
+                    };
+                };
+                class GVAR(Wiki) {
+                    text = CSTRING(ToolbarWiki);
+                    picture = "\a3\3DEN\Data\Controls\ctrlMenu\link_ca.paa";
+                    weblink = "https://wiki.armaforces.com/";
+                    opensNewWindow = 1;
                 };
             };
         };
