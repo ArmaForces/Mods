027fc6fd7fce3f3993ee9ce6a78bda4456480cf8
Filip Maciejewski <veteran29@users.noreply.github.com>
2020-07-19 23:21:47 +0200

    Add smoke component

---
+++ addons/smoke/script_component.hpp
@@ -0,0 +1,15 @@
+#define COMPONENT smoke
+#include "\z\afm\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_SMOKE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SMOKE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SMOKE
+#endif
+
+#include "\z\afm\addons\main\script_macros.hpp"
