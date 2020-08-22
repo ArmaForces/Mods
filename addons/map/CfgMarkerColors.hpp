85acf1311de2510c032f923e64fec7d09951d689
Filip Maciejewski <veteran29@users.noreply.github.com>
2020-08-22 02:22:54 +0200

    Add team designation description to map colors

---
+++ addons/map/CfgMarkerColors.hpp
@@ -0,0 +1,12 @@
+class CfgMarkerColors {
+    class Default;
+    class ColorRed: Default {
+        name = CSTRING(ColorRed);
+    };
+    class ColorBlue: Default {
+        name = CSTRING(ColorBlue);
+    };
+    class ColorGreen: Default {
+        name = CSTRING(ColorGreen);
+    };
+};
