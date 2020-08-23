809212030b604cbdb7fa7f454c79dcf28799392b
Filip Maciejewski <veteran29@users.noreply.github.com>
2020-08-23 15:43:10 +0200

    Increase map markers icon list height

--- addons/map/ui/RscDisplayMainMap.hpp
+++ addons/map/ui/RscDisplayMainMap.hpp
@@ -6,6 +6,12 @@ class RscDisplayMainMap {
         class TopRight: RscControlsGroup {
             class controls {
                 class MarkerColor: RscCombo {
+                    // idc = IDC_DIARY_MARKER_COLOR;
+                    wholeHeight = 0.6;
+                };
+                class MarkerIcon: RscCombo
+                {
+                    // idc = IDC_DIARY_MARKER_ICON;
                     wholeHeight = 0.6;
                 };
             };
