ffc1916afb5c3ee336c0b77ac13b3f68fd636c87
Filip Maciejewski <veteran29@users.noreply.github.com>
2020-08-23 16:02:28 +0200

    Add team designation description to map colors (#93)
    * Add team designation description to map colors
    
    * Unlock Brown, Orange and Pink marker colors
    
    * Remove brown color from map
    
    * Add Orange and Pink color designations
    
    * Adjust pink color
    
    * Increase map markers color list height
    
    * Increase map markers icon list height

---
+++ addons/map/XEH_preInit.sqf
@@ -0,0 +1,4 @@
+#include "script_component.hpp"
+ADDON = false;
+
+ADDON = true;
---
+++ addons/map/ui/RscDisplayMainMap.hpp
@@ -0,0 +1,20 @@
+class RscControlsGroup;
+class RscCombo;
+
+class RscDisplayMainMap {
+    class controls {
+        class TopRight: RscControlsGroup {
+            class controls {
+                class MarkerColor: RscCombo {
+                    // idc = IDC_DIARY_MARKER_COLOR;
+                    wholeHeight = 0.6;
+                };
+                class MarkerIcon: RscCombo
+                {
+                    // idc = IDC_DIARY_MARKER_ICON;
+                    wholeHeight = 0.6;
+                };
+            };
+        };
+    };
+};
