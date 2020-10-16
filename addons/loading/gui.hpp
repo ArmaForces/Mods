36b531cc1792b0083c9db3f2bf6810f9b7fa37b3
Filip Maciejewski <veteran29@users.noreply.github.com>
2020-10-16 04:38:46 +0200

    Remove double A3 logo if loading screen fails or no images in config

--- addons/loading/gui.hpp
+++ addons/loading/gui.hpp
@@ -3,37 +3,34 @@ class RscPicture;
 class RscStandardDisplay;
 class RscControlsGroup;
 class RscPictureKeepAspect;
-#define DISPLAY_LOADING_ADD_BG(displayClass) class displayClass: RscStandardDisplay { \
-    class controls { \
-        class LoadingStart: RscControlsGroup { \
-            class VScrollbar {\
-                width = 0; \
-            }; \
-            class controls { \
-                class GVAR(background): RscPictureKeepAspect { \
-                    idc = IDC_LOADINGSTART_CUSTOM_BG; \
-                    text = QPATHTOF(ui\loading\0_co.paa); \
-                    x = 0; \
-                    y = "-(0.5 * safezoneH)"; \
-                    w = "safezoneW"; \
-                    h = "2 * safezoneH"; \
+#define DISPLAY_LOADING_ADD_BG(displayClass) \
+    class displayClass: RscStandardDisplay { \
+        class controls { \
+            class LoadingStart: RscControlsGroup { \
+                class VScrollbar {\
+                    width = 0; \
                 }; \
-                class GVAR(lines): RscPicture { \
-                    text = "\A3\Ui_f\data\IGUI\RscTitles\SplashArma3\arma3_splashNoise_ca.paa"; \
-                    x = 0; \
-                    y = 0; \
-                    w = "safezoneW"; \
-                    h = "safezoneH"; \
-                }; \
-                class Logo; \
-                class GVAR(logo): Logo { \
-                    idc = -1; \
-                    y = "0 * safezoneH"; \
+                class controls { \
+                    class GVAR(background): RscPictureKeepAspect { \
+                        idc = IDC_LOADINGSTART_CUSTOM_BG; \
+                        x = 0; \
+                        y = "-(0.5 * safezoneH)"; \
+                        w = "safezoneW"; \
+                        h = "2 * safezoneH"; \
+                    }; \
+                    class Noise; \
+                    class GVAR(lines): Noise {}; \
+                    class Logo: RscPictureKeepAspect { \
+                        y = "-safeZoneH"; \
+                    }; \
+                    class GVAR(logo): Logo { \
+                        idc = -1; \
+                        y = "0 * safezoneH"; \
+                    }; \
                 }; \
             }; \
         }; \
-    }; \
-}
+    }
 
 DISPLAY_LOADING_ADD_BG(RscDisplayStart);
 DISPLAY_LOADING_ADD_BG(RscDisplayLoadMission);
