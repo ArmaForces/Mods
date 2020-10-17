8c71848f691736f2308a9b8c9e928c9f373236f3
Filip Maciejewski <veteran29@users.noreply.github.com>
2020-10-17 23:05:47 +0200

    Disable debug

--- addons/loading/script_component.hpp
+++ addons/loading/script_component.hpp
@@ -1,7 +1,7 @@
 #define COMPONENT loading
 #include "\z\afm\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
+// #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_LOADING
