894b3ac265fd2123e08a3e53c6b7bbc03b641d4d
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-01-11 00:41:00 +0100

    Add setting to disable remote sensors on clients

---
+++ addons/remotesensors/XEH_preInit.sqf
@@ -0,0 +1,6 @@
+#include "script_component.hpp"
+ADDON = false;
+
+#include "initSettings.sqf"
+
+ADDON = true;
---
+++ addons/remotesensors/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT remotesensors
+#include "\z\afm\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_REMOTESENSORS
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_REMOTESENSORS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_REMOTESENSORS
+#endif
+
+#include "\z\afm\addons\main\script_macros.hpp"
