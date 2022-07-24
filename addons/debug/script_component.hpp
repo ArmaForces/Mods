71eadd63e03c618591c693425ad83c512d9cdb77
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-07-24 02:06:57 +0200

    Remove debug component

--- addons/debug/script_component.hpp
+++
@@ -1,7 +0,0 @@
-#define COMPONENT debug
-#include "\z\afm\addons\main\script_mod.hpp"
-
-// #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-
-#include "\z\afm\addons\main\script_macros.hpp"
