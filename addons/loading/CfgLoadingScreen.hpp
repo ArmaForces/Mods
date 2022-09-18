7b85b214e96ecbcf64fcb192082fb9e5b88e25cd
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-09-18 17:24:26 +0200

    Add 4 new loading screens

--- addons/loading/CfgLoadingScreen.hpp
+++ addons/loading/CfgLoadingScreen.hpp
@@ -11,6 +11,7 @@ class GVAR(CfgLoadingScreen) {
         LOADING_SCREEN_CLASS(ifa_0,Mikkeboss);
         LOADING_SCREEN_CLASS(ifa_1,Krystol);
         LOADING_SCREEN_CLASS(ifa_2,veteran29);
+        LOADING_SCREEN_CLASS(ifa_3,ARR_2(Krystol, WINDOWS_95_SE));
         LOADING_SCREEN_CLASS(rhs_0,Monk[e]y);
         LOADING_SCREEN_CLASS(rhs_1,ARR_2(Mikkeboss, JurijPietrowicz));
         LOADING_SCREEN_CLASS(rhs_2,Mikkeboss);
@@ -18,11 +19,14 @@ class GVAR(CfgLoadingScreen) {
         LOADING_SCREEN_CLASS(rhs_4,ARR_2(Norek, Nomus));
         LOADING_SCREEN_CLASS(rhs_5,Bolec);
         LOADING_SCREEN_CLASS(rhs_6,Bolec);
+        LOADING_SCREEN_CLASS(rhs_7,Renchon);
+        LOADING_SCREEN_CLASS(rhs_8,Hyperion);
         LOADING_SCREEN_CLASS(vanilla_0,Monk[e]y);
         LOADING_SCREEN_CLASS(other_0,Monk[e]y);
         LOADING_SCREEN_CLASS(other_1,Monk[e]y);
         LOADING_SCREEN_CLASS(unsung_0,WINDOWS_95_SE);
         LOADING_SCREEN_CLASS(unsung_1,3Mydlo3);
         LOADING_SCREEN_CLASS(postapo_0,Monk[e]y);
+        LOADING_SCREEN_CLASS(bunny,Jabar);
     };
 };
