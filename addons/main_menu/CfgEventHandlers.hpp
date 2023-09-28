de4b10e52939695a0a4a65385d81f5f8b31865f8
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-09-28 15:25:05 +0200

    Add SQFC support

--- addons/main_menu/CfgEventHandlers.hpp
+++ addons/main_menu/CfgEventHandlers.hpp
@@ -1,28 +1,28 @@
 class Extended_PreStart_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_preStart));
+        init = QUOTE(call COMPILE_SCRIPT(XEH_preStart));
     };
 };
 class Extended_PreInit_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_preInit));
+        init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
     };
 };
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_postInit));
+        init = QUOTE(call COMPILE_SCRIPT(XEH_postInit));
     };
 };
 
 class Extended_DisplayLoad_EventHandlers {
     class RscDisplayRemoteMissions {
         ADDON = QUOTE(with uiNamespace do {\
-            ARR_2([_this select 0, 'RscDisplayRemoteMissions']) call COMPILE_FILE(XEH_multiplayerDisplay);\
+            ARR_2([_this select 0, 'RscDisplayRemoteMissions']) call COMPILE_SCRIPT(XEH_multiplayerDisplay);\
         });
     };
     class RscDisplayMultiplayerSetup {
         ADDON = QUOTE(with uiNamespace do {\
-            ARR_2([_this select 0, 'RscDisplayMultiplayerSetup']) call COMPILE_FILE(XEH_multiplayerDisplay);\
+            ARR_2([_this select 0, 'RscDisplayMultiplayerSetup']) call COMPILE_SCRIPT(XEH_multiplayerDisplay);\
         });
     };
 };
