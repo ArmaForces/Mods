1bae0ff382f1c96683b9e45e9a4808d4b21deee7
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-10-18 18:32:51 +0200

    Fix being able to move in static animations

--- addons/compatibility/kka3_ace_extension/CfgMoves.hpp
+++ addons/compatibility/kka3_ace_extension/CfgMoves.hpp
@@ -5,43 +5,43 @@ class CfgMovesMaleSdr: CfgMovesBasic {
 		class AmovPercMstpSnonWnonDnon;
 		class CutSceneAnimationBase;
 		class relax_44: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class relax_45: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class attention1: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class attention2: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class attention3: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class relax_tes: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class relax_21: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class relax_24: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class relax_3: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class relax_32: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class relax_8: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class relax_7: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 		class rattention2: CutSceneAnimationBase {
-			actions = "RifleStandActions";
+			actions = "CivilStandActions";
 		};
 	};
 };
