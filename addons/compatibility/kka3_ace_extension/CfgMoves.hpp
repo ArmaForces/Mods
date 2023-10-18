ffcfe3aa475723542dbb52205f5b05adcf4180b0
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-10-18 18:39:52 +0200

    Remove tabs

--- addons/compatibility/kka3_ace_extension/CfgMoves.hpp
+++ addons/compatibility/kka3_ace_extension/CfgMoves.hpp
@@ -1,47 +1,47 @@
 
 class CfgMovesBasic;
 class CfgMovesMaleSdr: CfgMovesBasic {
-	class States {
-		class AmovPercMstpSnonWnonDnon;
-		class CutSceneAnimationBase;
-		class relax_44: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class relax_45: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class attention1: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class attention2: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class attention3: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class relax_tes: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class relax_21: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class relax_24: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class relax_3: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class relax_32: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class relax_8: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class relax_7: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-		class rattention2: CutSceneAnimationBase {
-			actions = "CivilStandActions";
-		};
-	};
+    class States {
+        class AmovPercMstpSnonWnonDnon;
+        class CutSceneAnimationBase;
+        class relax_44: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class relax_45: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class attention1: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class attention2: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class attention3: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class relax_tes: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class relax_21: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class relax_24: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class relax_3: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class relax_32: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class relax_8: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class relax_7: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+        class rattention2: CutSceneAnimationBase {
+            actions = "CivilStandActions";
+        };
+    };
 };
