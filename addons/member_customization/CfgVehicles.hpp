c35554478cedcc8d77840621cc2207ec5b07eb3a
Filip Maciejewski <veteran29@users.noreply.github.com>
2024-01-07 21:43:16 +0100

    Use proper texture suffixes

--- addons/member_customization/CfgVehicles.hpp
+++ addons/member_customization/CfgVehicles.hpp
@@ -2,7 +2,7 @@ class CfgVehicles {
     class UserTexture1m_F;
     class GVAR(graffiti_chernarus_bolec_17kills): UserTexture1m_F {
         displayName = "bolec 17 kills graffiti";
-        hiddenSelectionsTextures[] = {QPATHTOF(data\chernarus_graffiti_bolec_17kills.paa)};
+        hiddenSelectionsTextures[] = {QPATHTOF(data\chernarus_graffiti_bolec_17kills_ca.paa)};
 
         class Attributes {};
     };
@@ -10,7 +10,7 @@ class CfgVehicles {
     class UserTexture_1x2_F;
     class GVAR(banner_chernarus_bolec_17kills): UserTexture_1x2_F {
         displayName = "bolec 17 kills banner";
-        hiddenSelectionsTextures[] = {QPATHTOF(data\chernarus_banner_bolec_17kills.paa)};
+        hiddenSelectionsTextures[] = {QPATHTOF(data\chernarus_banner_bolec_17kills_ca.paa)};
 
         class Attributes {};
     };
