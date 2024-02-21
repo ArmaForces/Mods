1c51400c3839a5de4cc0e99456376d185d138d3d
Filip Maciejewski <veteran29@users.noreply.github.com>
2024-02-21 23:17:03 +0100

    Spotlight Block - Add component (#212)

---
+++ addons/spotlight_block/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT spotlight_block
+#include "\z\afm\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_SPOTLIGHT_BLOCK
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_SPOTLIGHT_BLOCK
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SPOTLIGHT_BLOCK
+#endif
+
+#include "\z\afm\addons\main\script_macros.hpp"
