8c390a0fcc5c5816d35d91243116af2e7035fbe2
3Mydlo3 <mydlo333@interia.pl>
2024-08-14 18:45:11 +0200

    Create decorator out of RscDisplayEGSpectator

--- addons/spectator/CfgFunctions.hpp
+++ addons/spectator/CfgFunctions.hpp
@@ -12,6 +12,10 @@ class CfgFunctions
             {
                 file = QUOTE(A3\Functions_F_Exp_A\EGSpectator\fn_EGSpectator.sqf);
             };
+            class RscDisplayEGSpectatorCore
+            {
+                file = QUOTE(A3\Ui_F\Scripts\Gui\rscdisplayegspectator.sqfc);
+            };
         };
     };
 };
