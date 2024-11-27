d8d8b815962eda1b9bc27dcbb4c9c51d6026b4d4
Filip Maciejewski <veteran29@users.noreply.github.com>
2024-11-27 17:55:54 +0100

    Compatiblity - Fix Essekeer grid

---
+++ addons/compatibility/map_esseker/CfgWorlds.hpp
@@ -0,0 +1,11 @@
+class CfgWorlds {
+    class CAWorld;
+    class Stratis: CAWorld {
+        class Grid;
+    };
+    class Esseker: Stratis {
+        class Grid: Grid {
+            offsetY = 12288;
+        };
+    };
+};
